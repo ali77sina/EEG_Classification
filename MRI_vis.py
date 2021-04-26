106a64384781cafadac850e40fa3ef43c1e4d7e6
ali77sina <54308350+ali77sina@users.noreply.github.com>
2021-04-26 01:48:18 +0100

    Add files via upload

---
+++ MRI_vis.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Sina
+"""
+
+#simple code to visualise MRI slices
+
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+from obspy.imaging.cm import obspy_sequential
+import os
+import pydicom
+
+
+# img = nib.load(r'C:\Users\Sina\Desktop\head images\mni_icbm152_t2_tal_nlin_asym_09c.mnc')
+img = nib.load(r'C:\Users\Sina\Desktop\multiEEGMRI\ucl\sMRI\smri.img')
+data = img.get_fdata()
+print(data.mean())
+print(data.max())
+print(data.min())
+
+
+for i in range(193):      #loop to show sliced photos
+    plt.pcolormesh(data[i], cmap = 'gray')
+    plt.show()
+
